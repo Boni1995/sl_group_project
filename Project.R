40d2563d2a5261a3e060975683c15eb488fc1d83
Boni1995 <francobonifacini@hotmail.com>
2024-03-11 22:11:17 +0100

    datasets

--- Project.R
+++ Project.R
@@ -3,23 +3,56 @@ library(dplyr)
 
 # Load datasets
 
-matches <- read.csv("C:\\Users\\franc\\Documents\\GitHub\\sl_group_project\\Datasets\\matches_1930_2022.csv")
-immigration <- read.csv("C:\\Users\\franc\\Documents\\GitHub\\sl_group_project\\Datasets\\MIG_05032024164943281.csv")
+wc_matches <- read.csv("C:\\Users\\franc\\Documents\\GitHub\\sl_group_project\\Datasets\\matches_1930_2022.csv")
+matches <- read.csv("C:\\Users\\franc\\Documents\\GitHub\\sl_group_project\\Datasets\\results.csv")
+ds_migration <- read.csv("C:\\Users\\franc\\Documents\\GitHub\\sl_group_project\\Datasets\\MIG_05032024164943281.csv")
 
-ds_matches <- matches[,c(1:3, 6)]
+# Keep only teams' names and scores
+ds_matches <- matches[,c(2:5)]
 
+# Create a country index
+all_countries <- c(ds_matches$home_team, ds_matches$away_team, ds_migration$Country)
+unique_countries <- unique(all_countries)
+countries_index <- data.frame(Country = unique_countries) # Copy this column and use it in excel to double check with migration countries
+
+# Check availability of countries in each dataset
+countries_index$home <- ifelse(countries_index$Country %in% ds_matches$home_team, 1, 0)
+countries_index$away <- ifelse(countries_index$Country %in% ds_matches$away_team, 1, 0)
+countries_index$migration <- ifelse(countries_index$Country %in% ds_migration$Country, 1, 0)
+
+ruta_archivo <- "C:\\Users\\franc\\Documents\\GitHub\\sl_group_project\\Datasets\\check_country_index.xlsx"
+write.xlsx(countries_index, file = ruta_archivo)
+
+country_corrections <- c("Czechia"="Czech Republic", "Czechoslovakia"="Czech Republic",
+                         "Western Australia"="Australia","German DR"="Germany", "Northern Ireland"="Ireland",
+                         "Republic of Ireland"="Ireland", "South Korea"="Korea", "Slovak Republic"="Slovakia",
+                         "Central Spain"="Spain", "Turkey"="Türkiye", "United Kingdom"="England")
+
+ds_matches$home_team <- ifelse(ds_matches$home_team %in% names(country_corrections),
+                             country_corrections[ds_matches$home_team], ds_matches$home_team)
+
+ds_matches$away_team <- ifelse(ds_matches$away_team %in% names(country_corrections),
+                               country_corrections[ds_matches$away_team], ds_matches$away_team)
+
+ds_migration$Country <- ifelse(ds_migration$Country %in% names(country_corrections),
+                               country_corrections[ds_migration$Country], ds_migration$Country)
+
+# Add a column to define if the home team won, lost or tie (for away team, we will later use an inverse count)
 ds_matches$outcome <- ifelse(ds_matches$home_score > ds_matches$away_score, "w",
                              ifelse(ds_matches$home_score < ds_matches$away_score, "l", "t"))
 
-colSums(is.na(matches))
+# Create a dataset of immigration ("inflows of foreing population") and of
+# ("Acquisition of nationality by country of former nationality") from the migration dataset
 
-sum(immigration$Variable == 'Acquisition of nationality by country of former nationality')
+ds_migration_filter <- ds_migration[,c(4,8,11)]
 
-immigration_filter <-subset(immigration, Variable == "Acquisition of nationality by country of former nationality")
+immigration <- subset(ds_migration_filter, Variable == "Inflows of foreign population by nationality")
 
-ds_immigration <- immigration_filter[,c(7:8, 11)]
+nationality <- subset(ds_migration_filter, Variable == "Acquisition of nationality by country of former nationality")
 
-colnames(ds_immigration) <- c("code", "country", "nationality_acquisition")
+ds_immigration <- immigration[,c(2-3)]
+
+ds_nationality <- nationality[,c(2-3)]
 
 
 # Immigration dataset analysis
