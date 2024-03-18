db0f365cc9cccee4d0d808b8e0f115ac838fd132
Boni1995 <francobonifacini@hotmail.com>
2024-03-18 11:01:10 +0100

    ok

--- Final Dataset matches_foreign.R
+++ Final Dataset matches_foreign.R
@@ -1,6 +1,23 @@
 matches = read.csv('wc_output.csv')
 foreign_born = read.csv('foreign_born.csv')
 
+# Correct country names
+country_corrections <- c("Cameroon "="Cameroon", "China PR" = "China", "Czechoslavakia" = "Czechoslovakia",
+                         "East Germany" = "Germany", "IR Iran" = "Iran", "Côte d'Ivoire" = "Ivory Coast",
+                         "Korea DPR" = "North Korea", "Northern Ireland" = "Ireland", "Republic of Ireland" = "Ireland",
+                         "SFR Yugoslavia" = "Yugoslavia", "Korea Republic" = "South Korea", "Sweden " = "Sweden",
+                         "Turkey" = "Türkiye", "West Germany" = "Germany", "FR Yugoslavia" = "Yugoslavia",
+                         "Germany DR" = "Germany")
+
+matches$home_team <- ifelse(matches$home_team %in% names(country_corrections),
+                            country_corrections[matches$home_team], matches$home_team)
+
+matches$away_team <- ifelse(matches$away_team %in% names(country_corrections),
+                            country_corrections[matches$away_team], matches$away_team)
+
+foreign_born$International <- ifelse(foreign_born$International %in% names(country_corrections),
+                                     country_corrections[foreign_born$International], foreign_born$International)
+
 colnames(matches)
 colnames(foreign_born)
 
@@ -21,3 +38,7 @@ matches_foreign = matches_foreign %>%
 names(matches_foreign) = c("home_team", "away_team", "Year", "home_score", "away_score", "outcome", "home_foreign", "away_foreign")
 
 matches_foreign = na.omit(matches_foreign)
+
+library(openxlsx)
+path <- "C:\\Users\\franc\\Documents\\GitHub\\sl_group_project\\Datasets\\matches_foreign.csv"
+write.csv(matches_foreign, file = path)
